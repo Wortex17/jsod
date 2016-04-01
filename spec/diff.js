9317f992a6f3a3f7913e96177ba26f5883b30c03
Patrick Michael Hopf <phopf.mmt-m2013@fh-salzburg.ac.at>
2016-04-02 00:11:54 +0200

    Add test suite for diffing unordered value lists
    Test coverage for unordered value lists ~100%

--- spec/diff.js
+++ spec/diff.js
@@ -23,7 +23,322 @@ describe('jsod#diff()', function() {
     describeValueTypeDiffSuite("regex", gen.regex, gen.regexAlt);
     describeValueTypeDiffSuite("functions", gen.func, gen.funcAlt);
 
+    describeUnorderedValuelistDiffSuite();
+
 });
+
+function nest(object, nestDepth)
+{
+    let pivot = object;
+    for(let i = 0; i < nestDepth; i++)
+    {
+        pivot = {['lvl'+(nestDepth-i)]: pivot};
+    }
+    return pivot;
+}
+
+function describeUnorderedValuelistDiffSuite()
+{
+    describe("diffing arrays as unordered valuelist", function() {
+
+        let testCompilation = [
+            //These tests actually test that the structural diff also works correctly on unordered valuelists
+            [whenListHasBeenSet, 'when empty list has been set',  undefined, []],
+            [whenListHasBeenSet, 'when non-empty list has been set', undefined, ['a']],
+            [whenListHasBeenDeleted, 'when empty list has been deleted', [], undefined],
+            [whenListHasBeenDeleted, 'when non-empty list has been deleted', ['a'], undefined],
+            //The next tests are specific for unoredered valuelist diffing
+            [whenListWasNotChanged, 'when empty list was not changed', [], []],
+            [whenListWasNotChanged, 'when non-empty list was not changed', ['a'], ['a']],
+            [whenListWasNotChanged, 'when list contents were reordered', ['a', 'b'], ['b', 'a']],
+            [whenListContentWasAdded, 'when list contents were added to empty list', [], ['a']],
+            [whenListContentWasAdded, 'when list contents were added at the end', ['b'], ['b', 'a']],
+            [whenListContentWasAdded, 'when list contents were added at the front', ['b'], ['a', 'b']],
+            [whenListContentWasAdded, 'when duplicates of list contents were added', ['a'], ['a', 'a']],
+            [whenListContentWasDeleted, 'when list contents were deleted', ['a'], []],
+            [whenListContentWasDeleted, 'when one of many list contents were deleted', ['a', 'b'], ['b']],
+            [whenListContentWasDeleted, 'when one of duplicate list contents were deleted', ['a', 'a'], ['a']],
+            [whenListContentWasDeleted, 'when one of duplicate list contents aside non-dupes were deleted', ['a', 'a', 'b'], ['a', 'b']],
+            [whenListContentWasReplaced, 'when list contents were replaced', ['a'], ['b']]
+        ];
+        describe("direct array", function() {
+            _.forEach(testCompilation, function(test){
+                test[0](test[1], test[2], test[3]);
+            });
+        });
+        describe("array nested in object", function() {
+            _.forEach(testCompilation, function(test){
+                if(test[0] == whenListHasBeenSet || test[0] == whenListHasBeenDeleted)
+                {
+                    test[0](test[1], nest(test[2], 1), nest(test[3], 1), 1);
+                }
+            });
+        });
+        describe("array nested in object in object", function() {
+            _.forEach(testCompilation, function(test){
+                if(test[0] == whenListHasBeenSet || test[0] == whenListHasBeenDeleted)
+                {
+                    test[0](test[1], nest(test[2], 2), nest(test[3], 2), 2);
+                }
+            });
+        });
+    });
+
+
+    function whenListHasBeenSet(contextDesc, O, A, depth)
+    {
+        depth = _.isUndefined(depth) ? 0 : depth;
+        context(contextDesc, function () {
+            let diff = jsod.diff(O, A);
+            let b = A;
+            let diff2 = jsod.diff(O, A);
+            let levelname = 'level ??';
+
+            for(let d = 0; d <= depth; d++)
+            {
+                levelname = (d > 0) ? 'level ' + d : 'root level';
+
+                it('should return a DiffTree object at ' + levelname, function() {
+                    expect(diff).to.be.a('object');
+                });
+                it('should return no conflicts at ' + levelname, function() {
+                    expect(diff['!']).to.satisfy(_.isUndefined);
+                });
+                if(d == depth)
+                {
+                    it('should return no subtrees at ' + levelname, function () {
+                        expect(diff['/']).to.satisfy(_.isUndefined);
+                    });
+                } else {
+                    it('should return no records at ' + levelname, function() {
+                        expect(diff['.']).to.satisfy(_.isUndefined);
+                    });
+                    it('should return one subtree at ' + levelname, function() {
+                        expect(diff['/']).to.be.a('object');
+                        expect(Object.keys(diff['/'])).to.have.length(1);
+                        let keyname = Object.keys(diff['/'])[0];
+                        diff = diff['/'][keyname]; // Jump down
+                        A = A[keyname];
+                        O = O[keyname];
+                    });
+                }
+            }
+
+            it('should return one record at '+levelname, function () {
+                expect(diff['.']).to.be.a('array');
+                expect(diff['.']).to.have.length(1);
+                expect(diff['.'][0]).to.be.a('array');
+            });
+            if(depth == 0)
+            {
+                it('should record with undefined key name', function () {
+                    expect(diff['.'][0][0]).to.satisfy(_.isUndefined);
+                });
+            } else {
+                it('should record with property name as key name', function () {
+                    expect(diff['.'][0][0]).to.equal('lvl'+(depth));
+                });
+            }
+            it('should record a ADD operation', function () {
+                expect(diff['.'][0]).to.have.length(3);
+                expect(diff['.'][0][1]).to.equal(jsod.Attributes.DeltaOperation.ADD);
+            });
+            it('should record the new value', function () {
+                expect(diff['.'][0][2]).to.satisfy(function deepEqual(x){
+                    return _.isEqual(x, A);
+                });
+            });
+        });
+    }
+    function whenListHasBeenDeleted(contextDesc, O, A, depth)
+    {
+        depth = _.isUndefined(depth) ? 0 : depth;
+        context(contextDesc, function () {
+            let diff = jsod.diff(O, A);
+            let levelname = 'level ??';
+
+            for(let d = 0; d <= depth; d++)
+            {
+                levelname = (d > 0) ? 'level ' + d : 'root level';
+
+                it('should return a DiffTree object at ' + levelname, function() {
+                    expect(diff).to.be.a('object');
+                });
+                it('should return no conflicts at ' + levelname, function() {
+                    expect(diff['!']).to.satisfy(_.isUndefined);
+                });
+                if(d == depth)
+                {
+                    it('should return no subtrees at ' + levelname, function () {
+                        expect(diff['/']).to.satisfy(_.isUndefined);
+                    });
+                } else {
+                    it('should return no records at ' + levelname, function() {
+                        expect(diff['.']).to.satisfy(_.isUndefined);
+                    });
+                    it('should return one subtree at ' + levelname, function() {
+                        expect(diff['/']).to.be.a('object');
+                        expect(Object.keys(diff['/'])).to.have.length(1);
+                        let keyname = Object.keys(diff['/'])[0];
+                        diff = diff['/'][keyname]; // Jump down
+                        A = A[keyname];
+                        O = O[keyname];
+                    });
+                }
+            }
+
+
+            it('should return one record at '+levelname, function () {
+                expect(diff['.']).to.be.a('array');
+                expect(diff['.']).to.have.length(1);
+                expect(diff['.'][0]).to.be.a('array');
+            });
+            if(depth == 0)
+            {
+                it('should record with undefined key name', function () {
+                    expect(diff['.'][0][0]).to.satisfy(_.isUndefined);
+                });
+            } else {
+                it('should record with property name as key name', function () {
+                    expect(diff['.'][0][0]).to.equal('lvl'+(depth));
+                });
+            }
+            it('should record a DELETE operation', function() {
+                expect(diff['.'][0][1]).to.equal(jsod.Attributes.DeltaOperation.DELETE);
+            });
+            it('should not record the changed value', function() {
+                expect(diff['.'][0]).to.have.length(2);
+                expect(diff['.'][0][2]).to.satisfy(_.isUndefined);
+            });
+        });
+    }
+    function whenListWasNotChanged(contextDesc, O, A)
+    {
+        context(contextDesc, function () {
+            let diff = jsod.DiffHandlers.diffAsUnorderedValuelist(O, A);
+
+            it('should return a DiffTree object', function() {
+                expect(diff).to.be.a('object');
+            });
+            it('should return no records at root level', function() {
+                expect(diff['.']).to.satisfy(_.isUndefined);
+            });
+            it('should return no subtrees at root level', function() {
+                expect(diff['/']).to.satisfy(_.isUndefined);
+            });
+            it('should return no conflicts at root level', function() {
+                expect(diff['!']).to.satisfy(_.isUndefined);
+            });
+        });
+    }
+    function whenListContentWasAdded(contextDesc, O, A, depth)
+    {
+        context(contextDesc, function () {
+            let diff = jsod.DiffHandlers.diffAsUnorderedValuelist(O, A);
+
+            it('should return a DiffTree object', function() {
+                expect(diff).to.be.a('object');
+            });
+            it('should return no subtrees at root level', function() {
+                expect(diff['/']).to.satisfy(_.isUndefined);
+            });
+            it('should return no conflicts at root level', function() {
+                expect(diff['!']).to.satisfy(_.isUndefined);
+            });
+            it('should return one record at root level', function() {
+                expect(diff['.']).to.be.a('array');
+                expect(diff['.']).to.have.length(1);
+                expect(diff['.'][0]).to.be.a('array');
+            });
+
+            it('should record with numeric key name', function() {
+                expect(diff['.'][0][0]).to.satisfy(_.isInteger);
+            });
+            it('should record a ADD operation', function() {
+                expect(diff['.'][0]).to.have.length(3);
+                expect(diff['.'][0][1]).to.equal(jsod.Attributes.DeltaOperation.ADD);
+            });
+            it('should record the new value', function() {
+                expect(diff['.'][0][2]).to.equal('a');
+            });
+        });
+    }
+    function whenListContentWasDeleted(contextDesc, O, A, depth)
+    {
+        context(contextDesc, function () {
+            let diff = jsod.DiffHandlers.diffAsUnorderedValuelist(O, A);
+
+            it('should return a DiffTree object', function() {
+                expect(diff).to.be.a('object');
+            });
+            it('should return no subtrees at root level', function() {
+                expect(diff['/']).to.satisfy(_.isUndefined);
+            });
+            it('should return no conflicts at root level', function() {
+                expect(diff['!']).to.satisfy(_.isUndefined);
+            });
+            it('should return one record at root level', function() {
+                expect(diff['.']).to.be.a('array');
+                expect(diff['.']).to.have.length(1);
+                expect(diff['.'][0]).to.be.a('array');
+            });
+
+            it('should record with numeric key name', function() {
+                expect(diff['.'][0][0]).to.satisfy(_.isInteger);
+            });
+            it('should record a valued DELETE operation', function() {
+                expect(diff['.'][0]).to.have.length(3);
+                expect(diff['.'][0][1]).to.equal(jsod.Attributes.DeltaOperation.DELETE);
+            });
+            it('should record the old value', function() {
+                expect(diff['.'][0][2]).to.equal('a');
+            });
+        });
+    }
+    function whenListContentWasReplaced(contextDesc, O, A, depth)
+    {
+        context(contextDesc, function () {
+            let diff = jsod.DiffHandlers.diffAsUnorderedValuelist(O, A);
+
+            it('should return a DiffTree object', function() {
+                expect(diff).to.be.a('object');
+            });
+            it('should return no subtrees at root level', function() {
+                expect(diff['/']).to.satisfy(_.isUndefined);
+            });
+            it('should return no conflicts at root level', function() {
+                expect(diff['!']).to.satisfy(_.isUndefined);
+            });
+            it('should return two records at root level', function() {
+                expect(diff['.']).to.be.a('array');
+                expect(diff['.']).to.have.length(2);
+                expect(diff['.'][0]).to.be.a('array');
+            });
+
+            it('should record with numeric key name', function() {
+                expect(diff['.'][0][0]).to.satisfy(_.isInteger);
+            });
+            it('should record a valued DELETE operation', function() {
+                expect(diff['.'][0]).to.have.length(3);
+                expect(diff['.'][0][1]).to.equal(jsod.Attributes.DeltaOperation.DELETE);
+            });
+            it('should record the old value', function() {
+                expect(diff['.'][0][2]).to.equal('a');
+            });
+
+            it('should record with numeric key name', function() {
+                expect(diff['.'][1][0]).to.satisfy(_.isInteger);
+            });
+            it('should record a ADD operation', function() {
+                expect(diff['.'][1]).to.have.length(3);
+                expect(diff['.'][1][1]).to.equal(jsod.Attributes.DeltaOperation.ADD);
+            });
+            it('should record the new value', function() {
+                expect(diff['.'][1][2]).to.equal('b');
+            });
+        });
+    }
+}
 
 function describeValueTypeDiffSuite(readableName, generateValue, generateAltValue)
 {
